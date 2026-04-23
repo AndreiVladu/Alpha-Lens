7b1c518c8e84b416f421c8f0ade43265d8072e34
Vladulescu Andrei <96783069+AndreiVladu@users.noreply.github.com>
2026-04-23 21:58:09 +0300

    Add files via upload

--- foto.js
+++ foto.js
@@ -26,6 +26,89 @@ const weddingGalleries = {
     'ana-mihai': {
         title: 'Ana & Mihai',
         photos: [
+             'https://poze.b-cdn.net/9-DSC05693.jpg',
+            'https://poze.b-cdn.net/17-DSC05749.jpg',
+            'https://poze.b-cdn.net/18-DSC05750.jpg',
+            'https://poze.b-cdn.net/29-DSC05821.jpg',
+            'https://poze.b-cdn.net/27-DSC05813.jpg',
+            'https://poze.b-cdn.net/25-DSC05792.jpg',
+            'https://poze.b-cdn.net/13-DSC05728.jpg',
+            'https://poze.b-cdn.net/20-DSC05760.jpg',
+            'https://poze.b-cdn.net/22-DSC05771.jpg',
+            'https://poze.b-cdn.net/50-DSC05951.jpg'
+        ]
+    },
+    'maria-andrei': {
+        title: 'Maria & Andrei',
+        photos: [
+             'https://poze.b-cdn.net/DSC00575.jpg',
+            'https://poze.b-cdn.net/DSC00575.jpg',
+            'https://poze.b-cdn.net/DSC00542.jpg',
+              'https://poze.b-cdn.net/DSC00535.jpg',
+            'https://poze.b-cdn.net/DSC00503.jpg',
+              'https://poze.b-cdn.net/DSC00486.jpg',
+            'https://poze.b-cdn.net/DSC00456.jpg',
+              'https://poze.b-cdn.net/DSC00442.jpg',
+            'https://poze.b-cdn.net/DSC00435.jpg',
+              'https://poze.b-cdn.net/DSC00424.jpg',
+            'https://poze.b-cdn.net/DSC00430.jpg',
+              'https://poze.b-cdn.net/DSC00413.jpg',
+            'https://poze.b-cdn.net/DSC00411.jpg',
+           
+        ]
+    },
+    'elena-cristian': {
+        title: 'Elena & Cristian',
+        photos: [
+             'https://poze.b-cdn.net/DSC02579.jpg',
+            'https://poze.b-cdn.net/DSC02554.jpg',
+            'https://poze.b-cdn.net/DSC02544.jpg',
+            'https://poze.b-cdn.net/DSC02523.jpg',
+            'https://poze.b-cdn.net/DSC02499.jpg',
+            'https://poze.b-cdn.net/DSC02505.jpg',
+             'https://poze.b-cdn.net/DSC02484.jpg',
+            'https://poze.b-cdn.net/DSC02455.jpg',
+             'https://poze.b-cdn.net/DSC02465.jpg',
+            'https://poze.b-cdn.net/DSC02446.jpg',
+            'https://poze.b-cdn.net/DSC02437.jpg',
+            'https://poze.b-cdn.net/DSC02432.jpg',
+            'https://poze.b-cdn.net/DSC02425.jpg',
+          'https://poze.b-cdn.net/DSC02416.jpg',
+          'https://poze.b-cdn.net/DSC02411.jpg',
+          'https://poze.b-cdn.net/DSC02409.jpg',
+          'https://poze.b-cdn.net/DSC02407.jpg',
+          'https://poze.b-cdn.net/DSC02402.jpg',
+          'https://poze.b-cdn.net/DSC02395.jpg',
+          'https://poze.b-cdn.net/DSC02381.jpg',
+       
+        ]
+    },
+    'ioana-alexandru': {
+        title: 'Ioana & Alexandru',
+        photos: [
+            'https://poze.b-cdn.net/DSC08562.jpg',
+            'https://poze.b-cdn.net/DSC08537.jpg',
+            'https://poze.b-cdn.net/DSC08534.jpg',
+            'https://poze.b-cdn.net/DSC08494.jpg',
+            'https://poze.b-cdn.net/DSC08449.jpg',
+            'https://poze.b-cdn.net/DSC08439.jpg',
+             'https://poze.b-cdn.net/DSC08426.jpg',
+            'https://poze.b-cdn.net/DSC08422.jpg',
+             'https://poze.b-cdn.net/DSC08422.jpg',
+            'https://poze.b-cdn.net/DSC08370.jpg',
+            'https://poze.b-cdn.net/DSC08358.jpg',
+            'https://poze.b-cdn.net/DSC08353.jpg',
+            'https://poze.b-cdn.net/DSC08353.jpg',
+          'https://poze.b-cdn.net/DSC08353-3.jpg',
+          'https://poze.b-cdn.net/DSC08329.jpg',
+          'https://poze.b-cdn.net/DSC08289.jpg',
+          'https://poze.b-cdn.net/DSC08244.jpg',
+          'https://poze.b-cdn.net/DSC08195.jpg',
+        ]
+    },
+    'cristina-adrian': {
+        title: 'Cristina & Adrian',
+        photos: [
             'https://poze.b-cdn.net/DSC00537.jpg',
             'https://poze.b-cdn.net/DSC00418.jpg',
             'https://poze.b-cdn.net/DSC00438.jpg',
@@ -36,50 +119,62 @@ const weddingGalleries = {
             'https://poze.b-cdn.net/DSC01605.jpg'
         ]
     },
-    'maria-andrei': {
-        title: 'Maria & Andrei',
-        photos: [
-            'https://poze.b-cdn.net/vlcsnap-2026-02-02-22h44m09s478.png',
-            'https://poze.b-cdn.net/vlcsnap-2026-02-02-22h44m09s478.png',
-            'https://poze.b-cdn.net/vlcsnap-2026-02-02-22h44m09s478.png',
-            'https://poze.b-cdn.net/vlcsnap-2026-02-02-22h44m09s478.png',
-            'https://poze.b-cdn.net/vlcsnap-2026-02-02-22h44m09s478.png',
-            'https://poze.b-cdn.net/vlcsnap-2026-02-02-22h44m09s478.png'
-        ]
-    },
-    'elena-cristian': {
-        title: 'Elena & Cristian',
-        photos: [
-            'https://poze.b-cdn.net/vlcsnap-2026-02-02-22h44m09s478.png',
-            'https://poze.b-cdn.net/vlcsnap-2026-02-02-22h44m09s478.png',
-            'https://poze.b-cdn.net/vlcsnap-2026-02-02-22h44m09s478.png',
-            'https://poze.b-cdn.net/vlcsnap-2026-02-02-22h44m09s478.png',
-            'https://poze.b-cdn.net/vlcsnap-2026-02-02-22h44m09s478.png',
-            'https://poze.b-cdn.net/vlcsnap-2026-02-02-22h44m09s478.png'
-        ]
-    },
-    'ioana-alexandru': {
-        title: 'Ioana & Alexandru',
-        photos: [
-            'https://poze.b-cdn.net/vlcsnap-2026-02-02-22h44m09s478.png',
-            'https://poze.b-cdn.net/vlcsnap-2026-02-02-22h44m09s478.png',
-            'https://poze.b-cdn.net/vlcsnap-2026-02-02-22h44m09s478.png',
-            'https://poze.b-cdn.net/vlcsnap-2026-02-02-22h44m09s478.png',
-            'https://poze.b-cdn.net/vlcsnap-2026-02-02-22h44m09s478.png',
-            'https://images.unsplash.com/photo-1583939003579-730e3918a45a?w=1600&q=80'
-        ]
-    },
-    'cristina-adrian': {
+
+  'majorat1': {
         title: 'Cristina & Adrian',
         photos: [
-            'https://poze.b-cdn.net/vlcsnap-2026-02-02-22h44m09s478.png',
-            'https://poze.b-cdn.net/vlcsnap-2026-02-02-22h44m09s478.png',
-            'https://poze.b-cdn.net/vlcsnap-2026-02-02-22h44m09s478.png',
-            'https://poze.b-cdn.net/vlcsnap-2026-02-02-22h44m09s478.png',
-            'https://poze.b-cdn.net/vlcsnap-2026-02-02-22h44m09s478.png',
-            'https://poze.b-cdn.net/vlcsnap-2026-02-02-22h44m09s478.png'
+            'https://poze.b-cdn.net/DSC08193.jpg',
+            'https://poze.b-cdn.net/DSC08183.jpg',
+            'https://poze.b-cdn.net/DSC08162.jpg',
+            'https://poze.b-cdn.net/DSC08144.jpg',
+            'https://poze.b-cdn.net/DSC08142.jpg',
+            'https://poze.b-cdn.net/DSC08117.jpg',
+              'https://poze.b-cdn.net/DSC08081.jpg',
+            'https://poze.b-cdn.net/DSC08052.jpg',
+            'https://poze.b-cdn.net/DSC08048.jpg',
+            'https://poze.b-cdn.net/DSC08037.jpg',
+            'https://poze.b-cdn.net/DSC08030.jpg',
+              'https://poze.b-cdn.net/DSC08016.jpg',
+            'https://poze.b-cdn.net/DSC07998.jpg',
+            'https://poze.b-cdn.net/DSC07959.jpg',
+            'https://poze.b-cdn.net/DSC07942.jpg',
+            'https://poze.b-cdn.net/DSC07920.jpg',
+        
+            
+            
         ]
-    }
+    },
+
+      'majorat2': {
+        title: 'Cristina & Adrian',
+        photos: [
+            'https://poze.b-cdn.net/raul.marian.photography%20(1%20of%201).jpg',
+            'https://poze.b-cdn.net/raul.marian.photography%20(1%20of%201)-9.jpg',
+            'https://poze.b-cdn.net/raul.marian.photography%20(1%20of%201)-7.jpg',
+               'https://poze.b-cdn.net/raul.marian.photography%20(1%20of%201)-5.jpg',
+            'https://poze.b-cdn.net/raul.marian.photography%20(1%20of%201)-35.jpg',
+            'https://poze.b-cdn.net/raul.marian.photography%20(1%20of%201)-33.jpg',
+               'https://poze.b-cdn.net/raul.marian.photography%20(1%20of%201)-32.jpg',
+            'https://poze.b-cdn.net/raul.marian.photography%20(1%20of%201)-31.jpg',
+            'https://poze.b-cdn.net/raul.marian.photography%20(1%20of%201)-21.jpg',
+               'https://poze.b-cdn.net/raul.marian.photography%20(1%20of%201)-21.jpg',
+            'https://poze.b-cdn.net/raul.marian.photography%20(1%20of%201)-2.jpg',
+            'https://poze.b-cdn.net/raul.marian.photography%20(1%20of%201)-2.jpg',
+              'https://poze.b-cdn.net/raul.marian.photography%20(1%20of%201)-19.jpg',
+            'https://poze.b-cdn.net/raul.marian.photography%20(1%20of%201)-18.jpg',
+            'https://poze.b-cdn.net/raul.marian.photography%20(1%20of%201)-16.jpg',
+               'https://poze.b-cdn.net/raul.marian.photography%20(1%20of%201)-14.jpg',
+            'https://poze.b-cdn.net/raul.marian.photography%20(1%20of%201)-13.jpg',
+            'https://poze.b-cdn.net/raul.marian.photography%20(1%20of%201)-12.jpg',
+               'https://poze.b-cdn.net/raul.marian.photography%20(1%20of%201)-11.jpg',
+            'https://poze.b-cdn.net/raul.marian.photography%20(1%20of%201)-10.jpg',
+            '',
+          
+            
+        ]
+    },
+
+    
 };
 
 // VARIABILE GLOBALE
